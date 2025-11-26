7d3228754d732ca2b4dc5f71cd3db5c2730a0a7d
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-11-26 20:11:26 -0300

    Ex031

---
+++ Exercises/Ex031/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] vect = new Student[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"\nAluguel #{i}");
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Student(name, email);
+            }
+
+            Console.WriteLine("\nQuartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine($"{i}: {vect[i].Name}, {vect[i].Email}");
+                }
+            }
+        }
+    }
+}
---
+++ Exercises/Ex031/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex031
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Student(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
