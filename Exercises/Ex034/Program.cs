ef07a289f7bf4eb5fab615cbdb348f69289970e0
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-11-28 09:34:47 -0300

    Ex034

---
+++ Exercises/Ex034/Program.cs
@@ -0,0 +1,41 @@
+﻿// https://github.com/acenelio/matrix2-csharp
+using System;
+
+namespace Ex034
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int m = int.Parse(values[0]);
+            int n = int.Parse(values[1]);
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] row = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(row[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine($"Position: {i}, {j}:");
+                        if (j > 0) Console.WriteLine($"Left: {mat[i, j - 1]}");
+                        if (j < n - 1) Console.WriteLine($"Right: {mat[i, j + 1]}");
+                        if (i > 0) Console.WriteLine($"Up: {mat[i - 1, j]}");
+                        if (i < m - 1) Console.WriteLine($"Down: {mat[i + 1, j]}");
+                    }
+                }
+            }
+        }
+    }
+}
