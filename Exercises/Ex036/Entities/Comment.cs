1a2ce52af29738c46b93e00701bfaaf7595844ab
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-12-02 09:20:21 -0300

    Ex036
    StringBuilder

---
+++ Exercises/Ex036/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex036.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() 
+        { 
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
