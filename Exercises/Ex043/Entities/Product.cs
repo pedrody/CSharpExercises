85910b6eadaa627119ec827746cea1812e07b641
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-12-07 12:33:09 -0300

    Ex043
    Working with files

---
+++ Exercises/Ex043/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex043.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Exercises/Ex043/Program.cs
@@ -0,0 +1,41 @@
+﻿// https://github.com/acenelio/files1-csharp
+using System.Globalization;
+using Ex043.Entities;
+
+namespace Ex043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("File path to your CSV file: ");
+            string inputFile = Path.GetFullPath(Console.ReadLine());
+            string outputPath = Path.Combine(Path.GetDirectoryName(inputFile) + @"\out");
+            string outputFile = Path.Combine(outputPath + @"\summary.csv");
+            Directory.CreateDirectory(outputPath);
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(inputFile))
+                {
+                    using (StreamWriter sw = new StreamWriter(outputFile))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string[] line = sr.ReadLine().Split(',');
+                            Product item = new Product(
+                                line[0], double.Parse(line[1], 
+                                CultureInfo.InvariantCulture), 
+                                int.Parse(line[2]));
+                            sw.WriteLine(item.Name + "," + item.Total().ToString("F2", CultureInfo.InvariantCulture));
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred: " + e.Message);
+            }
+        }
+    }
+}
