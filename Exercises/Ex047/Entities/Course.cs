c1a0e676e1f33e56bb071fc46d6b3dc736949853
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-12-12 18:40:29 -0300

    Ex047
    HashSet

---
+++ Exercises/Ex047/Entities/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex047.Entities
+{
+    internal class Course
+    {
+        public string Name { get; set; }
+        public HashSet<int> Students { get; set; }
+
+        public Course(string name)
+        {
+            Name = name;
+            Students = new HashSet<int>();  // try without this statement
+        }
+    }
+}
---
+++ Exercises/Ex047/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://github.com/acenelio/set2-csharp
+using Ex047.Entities;
+
+namespace Ex047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course[] courses = { new Course("A"), new Course("B"), new Course("C") };
+            HashSet<int> totalStudent = new HashSet<int>();
+            foreach (Course course in courses)
+            {
+                Console.Write($"How many students for course {course.Name}? ");
+                int n = int.Parse(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    int student = int.Parse(Console.ReadLine());
+                    course.Students.Add(student);
+                }
+                totalStudent.UnionWith(course.Students);
+            }
+            Console.WriteLine("Total students: " + totalStudent.Count);
+        }
+    }
+}
