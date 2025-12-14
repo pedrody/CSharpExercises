0a3369108f38f4d035177b04dc051707192fdfa3
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-12-13 23:26:45 -0300

    Ex048
    Dictionary

---
+++ Exercises/Ex048/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace Ex048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string filePath = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(Path.GetFullPath(filePath)))
+                {
+                    Dictionary<string, int> dict = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string candidate = line[0];
+                        int number = int.Parse(line[1]);
+
+                        if (dict.ContainsKey(candidate))
+                        {
+                            dict[candidate] += number;
+                        }
+                        else
+                        {
+                            dict[candidate] = number;
+                        }
+                    }
+
+                    foreach (KeyValuePair<string, int> item in dict)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred: " + e.Message);
+            }
+        }
+    }
+}
